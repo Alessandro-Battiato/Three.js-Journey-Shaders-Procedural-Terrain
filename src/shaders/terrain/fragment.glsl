abf275bb1b1347f4becb8e02b4e165e7c4f053ed
alessandrobattiato <alessandroDev@outlook.it>
2024-06-15 15:55:36 +0200

    feat: water implemented

--- src/shaders/terrain/fragment.glsl
+++ src/shaders/terrain/fragment.glsl
@@ -5,9 +5,15 @@ uniform vec3 uColorGrass;
 uniform vec3 uColorSnow;
 uniform vec3 uColorRock;
 
+varying vec3 vPosition;
+
 void main() {
     // Color
     vec3 color = vec3(1.0);
+
+    // Water
+    float surfaceWaterMix = smoothstep(- 1.0, - 0.1, vPosition.y);
+    color = mix(uColorWaterDeep, uColorWaterSurface, surfaceWaterMix);
 
     // Final color
     csm_DiffuseColor = vec4(color, 1.0);
