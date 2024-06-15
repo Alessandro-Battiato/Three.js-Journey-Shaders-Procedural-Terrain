399ba1d2623e34adb642498f2f5dd302718b3bed
alessandrobattiato <alessandroDev@outlook.it>
2024-06-15 15:52:50 +0200

    feat: custom colors implemented

--- src/shaders/terrain/fragment.glsl
+++ src/shaders/terrain/fragment.glsl
@@ -1,3 +1,14 @@
+uniform vec3 uColorWaterDeep;
+uniform vec3 uColorWaterSurface;
+uniform vec3 uColorSand;
+uniform vec3 uColorGrass;
+uniform vec3 uColorSnow;
+uniform vec3 uColorRock;
+
 void main() {
-    
+    // Color
+    vec3 color = vec3(1.0);
+
+    // Final color
+    csm_DiffuseColor = vec4(color, 1.0);
 }
